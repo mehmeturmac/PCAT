63d2261116ae7ede6e07bc8a809c939efc720c5f
mehmeturmac <mehmeturmac@gmail.com>
2022-06-12 05:46:48 +0300

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = () => {
-  console.log("Test");
-};
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const photo = {
+    id: 1,
+    name: "Photo Name",
+    description: "Photo description",
+  };
+  res.send(photo);
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
