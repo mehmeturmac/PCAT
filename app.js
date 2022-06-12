13070167bcab203e285793e9de3688a09cb2ac30
mehmeturmac <mehmeturmac@gmail.com>
2022-06-12 06:17:45 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  const photo = {
-    id: 1,
-    name: "Photo Name",
-    description: "Photo description",
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
 });
 
 app.listen(port, () => {
