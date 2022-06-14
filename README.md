a742f70976a8c6d3ba716d5e87cbf98af10a9799
mehmeturmac <mehmeturmac@gmail.com>
2022-06-14 06:28:28 +0300

    uploading images with express-fileupload

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # PCAT
+
+## PCAT Projesi
+
+> Bu repo [Patika.dev](https://www.patika.dev/tr)'in hazırlamış olduğu [Node.js](https://app.patika.dev/courses/nodejs) eğitim patikasının [PCAT Projesi](https://app.patika.dev/courses/nodejs/PCATProject) için oluşturulmuştur.
+
+---
+
+![PCAT](/public/img/PCAT.png)
+
+---
+
+## Kullanılan Kaynaklar ve Paketler
+
+- [PCAT Proje Başlangıç Dosyaları](https://drive.google.com/file/d/1GxuCKjzE_W0EfOeZBUy_pEBm2rhyFxUC/view) ya da [templatemo.com](https://templatemo.com/tm-552-video-catalog)
+
+- [express](https://www.npmjs.com/package/express)
+
+- [nodemon](https://www.npmjs.com/package/nodemon)
+
+- [ejs](https://www.npmjs.com/package/ejs)
+
+- [mongoose](https://www.npmjs.com/package/mongoose)
+
+- [express-fileupload](https://www.npmjs.com/package/express-fileupload)
+
+- fs
+
+---
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+[Patika](https://www.patika.dev)
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
 const ejs = require("ejs");
+const fs = require("fs");
 const Photo = require("./models/Photo");
 
 const app = express();
@@ -18,10 +20,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 //ROUTES
 app.get("/", async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort("-dateCreated");
   res.render("index", { photos });
 });
 
@@ -39,11 +42,22 @@ app.get("/add", (req, res) => {
 });
 
 app.post("/photos", async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect("/");
+  const uploadDir = "public/uploads";
+  if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + "/public/uploads/" + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: "/uploads/" + uploadedImage.name,
+    });
+    res.redirect("/");
+  });
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı.`);
+  console.log(`Server started on port: ${port}`);
 });
