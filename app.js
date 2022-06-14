19a0f5927bc3d1a876e6b25b9f1e701d5385e917
mehmeturmac <mehmeturmac@gmail.com>
2022-06-14 07:39:36 +0300

    delete button updated

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ const Photo = require("./models/Photo");
 const app = express();
 
 //CONNECT DB
-mongoose.connect("mongodb://localhost/pcat-test-db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://localhost/pcat-test-db");
 
 //TEMPLATE ENGINE
 app.set("view engine", "ejs");
@@ -22,7 +19,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride("_method"));
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
 //ROUTES
 app.get("/", async (req, res) => {
@@ -70,6 +67,14 @@ app.put("/photos/:id", async (req, res) => {
   photo.description = req.body.description;
   await photo.save();
   res.redirect(`/photos/${req.params.id}`);
+});
+
+app.delete("/photos/:id", async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + "/public" + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect("/");
 });
 
 const port = 3000;
